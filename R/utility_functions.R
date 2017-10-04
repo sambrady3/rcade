12c7d7964570f9092e80cdfc5ea9335c90bccaab
Sam Brady <sambrady3@gmail.com>
2017-10-03 20:14:20 -0400

    Add generate_combined_dataset function

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -2,7 +2,7 @@
 #' Generate a uniform distribution bounded by the values of an input vector,
 #' with the same length as the input vector.
 #'
-#' @param x A vector.
+#' @param x A vector or dataframe.
 #'
 #' @return A vector representing a uniform distribution bounded by the values of \code{x},
 #' with the same length as \code{x}, and the same type as \code{x}
@@ -16,7 +16,7 @@ generate_uniform <- function(x) {
 }
 
 generate_uniform.numeric <- function(x) {
-  runif(length(x), min(x, max(x)))
+  runif(length(x), min(x), max(x))
 }
 
 generate_uniform.integer <- function(x) {
@@ -31,7 +31,20 @@ generate_uniform.character <- function(x) {
   sample(unique(x), length(x), replace=TRUE)
 }
 
+generate_uniform.data.frame <- function(x) {
+  as_data_frame(lapply(x[complete.cases(x), ], generate_uniform))
+}
+
 generate_uniform.default <- function(x) {
   sample(unique(x), length(x), replace=TRUE)
-  sample(unique(x), length(x), replace=TRUE)
+}
+
+generate_combined_dataset <- function(x, indicator_column = "fake") {
+  x_copy <- data.table::copy(x)
+  x_copy[indicator_column] <- 0
+
+  fake <- generate_uniform(x)
+  fake[indicator_column] <- 1
+
+  rbind(x_copy, fake)
 }
